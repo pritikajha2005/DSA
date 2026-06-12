1fd138be787bd1b4bb94cefe39617efae54a60f4
Pritika Jha <pritikajha07@gmail.com>
2026-06-13 00:57:39 +0530

    Added left rotate by k places (brute)

---
+++ Arrays/left_rotate_by_k.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+void leftRotateByK(vector<int>& arr, int k){
+    int n=arr.size();
+    k=k%n;
+
+    vector<int> temp;
+    for(int i=0; i<k; i++){
+        temp.push_back(arr[i]);
+    }
+
+    for(int i=k; i<n; i++){
+        arr[i-k]=arr[i];
+    }
+
+    for(int i=0; i<k; i++){
+        arr[n-k+i]=temp[i];
+    }
+}
+
+int main(){
+    vector<int> arr={1,2,3,4,5,6};
+    int k=3;
+    leftRotateByK(arr, k);
+
+    for(int i=0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    } 
+
+    return 0;
+}
