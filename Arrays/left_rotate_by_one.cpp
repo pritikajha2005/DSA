a7ebfa1e892e799a87f7fd697c5e06efb9fcd672
Pritika Jha <pritikajha07@gmail.com>
2026-06-13 00:33:54 +0530

    Added left rotate by one solution

---
+++ Arrays/left_rotate_by_one.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+void leftRotateByOne(vector<int>& arr){
+    int n=arr.size();
+
+    int first=arr[0];
+
+    for(int i=1; i<n; i++){
+        arr[i-1]=arr[i];
+    }
+
+    arr[n-1]=first;
+}
+
+int main(){
+    vector<int> arr={1,2,3,4,5};
+
+    leftRotateByOne(arr);
+    for(int i=0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    } 
+    return 0;
+}
