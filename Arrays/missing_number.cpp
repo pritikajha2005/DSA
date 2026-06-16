ff9520bf71ce6e9827d9132a399bd6c5b588133d
Pritika Jha <pritikajha07@gmail.com>
2026-06-16 23:22:02 +0530

    Added missing number solution

---
+++ Arrays/missing_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int missing_number(vector<int>& arr, int n){
+    int totalSum=n*(n+1)/2;
+    int arraySum=0;
+
+    for(int i=0; i<arr.size(); i++){
+        arraySum+=arr[i];
+    }
+
+    int missingNumber=totalSum-arraySum;
+
+    return missingNumber;
+}
+
+int main(){
+    vector<int> arr={1,2,3,4,6};
+    int n=6;
+    
+    cout<<missing_number(arr, n);
+    return 0;
+}
